0fe01f7d3e200ed2b43147655946edf3665a6834
rafaxyzgamer <131929400+rafaxyzgamer@users.noreply.github.com>
2024-03-18 21:38:40 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function login(){
+    window.location = "index.amarelo.html";
+}
+function login(){
+    window.location = "index.laaranja.html";
+}
+function login(){
+    window.location = "index.determinacao.html";
+}
+function login(){
+    window.location = "index.roxo.html";
+}
+function login(){
+    window.location = "index.azul.html";
+}
+function login(){
+    window.location = "index.paciencia.html";
+}
